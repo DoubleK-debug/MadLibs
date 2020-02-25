6226529f6d330c2ee7f4811c18ce26e297621859
Kaniel <Kaniel.kusi.2022@student.lhschools.org>
2020-02-25 11:51:17 -0500

    intellij commit

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,111 @@
+package com.LickingHeights;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Phase 1
+        String wordendinginING; // declared a variable
+        String favoriteVideoGame;
+        String location;
+        String feelingForHunger;
+        String verb;
+        String Noun;
+        String resturantName;
+        String favoriteMenuItem;
+        String properNoun;
+        String adjective;
+        String pluralNoun;
+        String emotion;
+        String adverb;
+        String endsWithING;
+        String noun;
+        String entranceIntoYourHouse;
+        String houseItem;
+        String verbEndingWithING;
+        String theWeatherOutside;
+        String emotionOfFrustraion;
+
+        //Phase 2
+        Scanner keyboard;
+
+        keyboard= new Scanner(System.in);
+        System.out.println("Give a word ending with ing");
+        wordendinginING = keyboard.nextLine();
+
+        System.out.println("Give me a favorite video game");
+        favoriteVideoGame = keyboard.nextLine();
+
+        System.out.println("Give me a location");
+        location = keyboard.nextLine();
+
+        System.out.println("Give me a feeling for hunger");
+        feelingForHunger= keyboard.nextLine();
+
+        System.out.println("Give me a verb");
+        verb  = keyboard.nextLine();
+
+        System.out.println("Give me a noun");
+        noun = keyboard.nextLine();
+
+        System.out.println("Give me a resturant name");
+        resturantName = keyboard.nextLine();
+
+        System.out.println("Give me a favortie menu item");
+        favoriteMenuItem = keyboard.nextLine();
+
+        System.out.println("Give me a proper noun");
+        properNoun = keyboard.nextLine();
+
+        System.out.println("Give me a adjective");
+        adjective = keyboard.nextLine();
+
+        System.out.println("Give me plural noun");
+        pluralNoun = keyboard.nextLine();
+
+        System.out.println("Give me a emotion");
+        emotion = keyboard.nextLine();
+
+        System.out.println("Give me a adverb");
+        adverb = keyboard.nextLine();
+
+        System.out.println("give me a ends with ing");
+        endsWithING = keyboard.nextLine();
+
+        System.out.println("Give me a noun");
+        noun = keyboard.nextLine();
+
+        System.out.println("Give me a entrance in to your house");
+        entranceIntoYourHouse = keyboard.nextLine();
+
+        System.out.println("Give me a house item");
+        houseItem = keyboard.nextLine();
+
+        System.out.println("Give me a verb ending with ing");
+        verbEndingWithING = keyboard.nextLine();
+
+        System.out.println("Give me the weather outside");
+        theWeatherOutside = keyboard.nextLine();
+
+        System.out.println("Give me a emotion of frustraion");
+        emotionOfFrustraion = keyboard.nextLine();
+
+
+
+        System.out.println();
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
+
